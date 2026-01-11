834e31f1db08f4e6c9569a5571e5984e18e16ca1
Dominic Zirbel <dominiczirbel@gmail.com>
2026-01-11 00:25:40 -0800

    Flesh out `TasksTest` with the full set of supported configurations

--- plugin/src/test/kotlin/io/github/dzirbel/TasksTest.kt
+++ plugin/src/test/kotlin/io/github/dzirbel/TasksTest.kt
@@ -3,10 +3,18 @@ package io.github.dzirbel
 import org.gradle.api.Task
 import org.gradle.api.tasks.TaskContainer
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
 import org.gradle.testfixtures.ProjectBuilder
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.assertContains
 
+/**
+ * Verifies task dependencies on projects created via [ProjectBuilder] (not TestKit projects built by test resource
+ * configuration files). This only verifies the task structure but has the advantage for KMP projects of not needing the
+ * system to match the project configuration, e.g. it can verify configuration for iOS native targets on non-macOS
+ * machines.
+ */
 class TasksTest {
 
     private val project = ProjectBuilder.builder().build()
@@ -14,7 +22,8 @@ class TasksTest {
     @Test
     fun `no other plugins`() {
         project.apply(plugin = "io.github.dzirbel.detekt-config")
-        assertEquals(
+
+        assertSameContents(
             listOf(":detekt", ":detektBaseline", ":detektGenerateConfig"),
             project.tasks.map { it.path },
         )
@@ -23,26 +32,131 @@ class TasksTest {
     @Test
     fun `after kotlin jvm plugin`() {
         project.apply(plugin = "org.jetbrains.kotlin.jvm")
-        assertEquals(setOf(":test"), project.tasks.check.dependencyPaths())
+        assertSameContents(listOf(":test"), project.tasks.check.dependencyPaths())
 
         project.apply(plugin = "io.github.dzirbel.detekt-config")
-        assertEquals(setOf(":detekt", ":test"), project.tasks.check.dependencyPaths())
-        assertEquals(setOf(":detektMain", ":detektTest"), project.tasks.detekt.dependencyPaths())
+
+        assertSameContents(listOf(":detekt", ":test"), project.tasks.check.dependencyPaths())
+        assertSameContents(listOf(":detektMain", ":detektTest"), project.tasks.detekt.dependencyPaths())
     }
 
     @Test
     fun `before kotlin jvm plugin`() {
         project.apply(plugin = "io.github.dzirbel.detekt-config")
         project.apply(plugin = "org.jetbrains.kotlin.jvm")
-        assertEquals(setOf(":detekt", ":test"), project.tasks.check.dependencyPaths())
-        assertEquals(setOf(":detektMain", ":detektTest"), project.tasks.detekt.dependencyPaths())
+
+        assertSameContents(listOf(":detekt", ":test"), project.tasks.check.dependencyPaths())
+        assertSameContents(listOf(":detektMain", ":detektTest"), project.tasks.detekt.dependencyPaths())
     }
 
-    // TODO KMP/JS tests
+    @Test
+    fun `after kotlin multiplatform jvm plugin`() {
+        project.apply(plugin = "org.jetbrains.kotlin.multiplatform")
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            jvm()
+        }
+        project.apply(plugin = "io.github.dzirbel.detekt-config")
+
+        assertContains(project.tasks.check.dependencyPaths(), ":detekt")
+        assertSameContents(listOf(":detektJvmMain", ":detektJvmTest"), project.tasks.detekt.dependencyPaths())
+    }
+
+    @Test
+    fun `after kotlin multiplatform js plugin`() {
+        project.apply(plugin = "org.jetbrains.kotlin.multiplatform")
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            js()
+        }
+        project.apply(plugin = "io.github.dzirbel.detekt-config")
+
+        assertContains(project.tasks.check.dependencyPaths(), ":detekt")
+        assertSameContents(listOf(":detektJsMain", ":detektJsTest"), project.tasks.detekt.dependencyPaths())
+    }
+
+    @Test
+    fun `after kotlin multiplatform native plugin, windows`() {
+        project.apply(plugin = "org.jetbrains.kotlin.multiplatform")
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            mingwX64()
+        }
+        project.apply(plugin = "io.github.dzirbel.detekt-config")
+
+        assertContains(project.tasks.check.dependencyPaths(), ":detekt")
+        assertSameContents(listOf(":detektMingwX64Main", ":detektMingwX64Test"), project.tasks.detekt.dependencyPaths())
+    }
+
+    @Test
+    fun `after kotlin multiplatform native plugin, ios`() {
+        project.apply(plugin = "org.jetbrains.kotlin.multiplatform")
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            iosX64()
+            iosArm64()
+            iosSimulatorArm64()
+        }
+        project.apply(plugin = "io.github.dzirbel.detekt-config")
+
+        assertContains(project.tasks.check.dependencyPaths(), ":detekt")
+        assertSameContents(
+            listOf(
+                ":detektIosArm64Main",
+                ":detektIosArm64Test",
+                ":detektIosSimulatorArm64Main",
+                ":detektIosSimulatorArm64Test",
+                ":detektIosX64Main",
+                ":detektIosX64Test",
+            ),
+            project.tasks.detekt.dependencyPaths(),
+        )
+    }
+
+    @Test
+    fun `after kotlin multiplatform native plugin, linux arm64`() {
+        project.apply(plugin = "org.jetbrains.kotlin.multiplatform")
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            linuxArm64()
+        }
+        project.apply(plugin = "io.github.dzirbel.detekt-config")
+
+        assertContains(project.tasks.check.dependencyPaths(), ":detekt")
+        assertSameContents(
+            listOf(":detektLinuxArm64Main", ":detektLinuxArm64Test"),
+            project.tasks.detekt.dependencyPaths(),
+        )
+    }
+
+    @Test
+    fun `after kotlin multiplatform native plugin, linux x64`() {
+        project.apply(plugin = "org.jetbrains.kotlin.multiplatform")
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            linuxX64()
+        }
+        project.apply(plugin = "io.github.dzirbel.detekt-config")
+
+        assertContains(project.tasks.check.dependencyPaths(), ":detekt")
+        assertSameContents(
+            listOf(":detektLinuxX64Main", ":detektLinuxX64Test"),
+            project.tasks.detekt.dependencyPaths(),
+        )
+    }
+
+    @Test
+    fun `after kotlin multiplatform jvm js plugin`() {
+        project.apply(plugin = "org.jetbrains.kotlin.multiplatform")
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            jvm()
+            js()
+        }
+        project.apply(plugin = "io.github.dzirbel.detekt-config")
+
+        assertContains(project.tasks.check.dependencyPaths(), ":detekt")
+        assertSameContents(
+            listOf(":detektJvmMain", ":detektJsMain", ":detektJvmTest", ":detektJsTest"),
+            project.tasks.detekt.dependencyPaths(),
+        )
+    }
 
     private val TaskContainer.check: Task get() = getByPath(":check")
     private val TaskContainer.detekt: Task get() = getByPath(":detekt")
 
-    private fun Task.dependencyPaths(): Set<String> =
-        taskDependencies.getDependencies(this).mapTo(mutableSetOf()) { it.path }
+    private fun Task.dependencyPaths(): Iterable<String> = taskDependencies.getDependencies(this).map { it.path }
 }
